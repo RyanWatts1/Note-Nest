fa51167fbb90a6773463ad549270a72149ca5231
RyanWatts1 <devshop202@yahoo.com>
2023-01-16 16:44:08 -0500

    paragraph feature

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useCallback } from "react";
 
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
-import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
